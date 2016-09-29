2f1a67e11bd09709f3225b95160985d0730daa09
Chirag <chiragl.spaceo@gmail.com>
2016-09-29 10:26:37 +0530

    Upload siri example code.

---
+++ SiriExampleTests/SiriExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  SiriExampleTests.swift
+//  SiriExampleTests
+//
+//  Created by Amrit Trivedi on 29/09/16.
+//  Copyright © 2016 Amrit Trivedi. All rights reserved.
+//
+
+import XCTest
+@testable import SiriExample
+
+class SiriExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
